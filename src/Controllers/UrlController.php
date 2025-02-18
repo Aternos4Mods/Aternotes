bc72033cd3f9fcd1320994de2b330e15f4f80730
Joshua Slui <joshua@dynamatrix.io>
2025-02-18 23:43:40 +0100

    Delete obsolete url controller

--- src/Controllers/UrlController.php
+++
@@ -1,9 +0,0 @@
-<?php
-function urlHandler(string $url): string
-{
-    if ($_ENV['PRODUCTION'] and $_ENV['PRODUCTION'] === 'true') {
-        return explode(".", $url)[0];
-    }
-
-    return $url;
-}
